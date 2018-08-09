7578d3f940a32e17b235bba0107c9ce71499af5e
torrar <art.torres.alv@gmail.com>
2018-08-08 23:22:02 -0500

    Exception definitions

---
+++ MagnetArgs/Exceptions/MagnetException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MagnetArgs
+{
+    public class MagnetException : Exception
+    {
+        public MagnetException()
+        {
+        }
+
+        public MagnetException(string message)
+            : base(message)
+        {
+        }
+
+        public MagnetException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
