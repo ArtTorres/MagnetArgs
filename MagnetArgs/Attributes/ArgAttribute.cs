7e01047c77d08f1ad9ac5e34e967eb2b6d4eb194
torrar <art.torres.alv@gmail.com>
2018-08-09 18:59:30 -0500

    Option and Arguments definition

---
+++ MagnetArgs/Attributes/ArgAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MagnetArgs
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true)]
+    public class ArgAttribute : Attribute
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public string Alias
+        {
+            get;
+            set;
+        }
+        //public bool IfPresent
+        //{
+        //    get;
+        //    set;
+        //}
+        //public bool IsRequired
+        //{
+        //    get;
+        //    set;
+        //}
+
+        //public object Parser
+        //{
+        //    get;
+        //    set;
+        //}
+        //public string DefaultValue
+        //{
+        //    get;
+        //    set;
+        //}
+
+        public ArgAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ MagnetArgs/Options/IOption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagnetArgs
+{
+    public interface IOption
+    {
+        int Order
+        {
+            get;
+            set;
+        }
+
+        //IEnumerable<HelpAttribute> Help
+        //{
+        //    get;
+        //    set;
+        //}
+
+        IEnumerable<Exception> Exceptions
+        {
+            get;
+            set;
+        }
+    }
+}
