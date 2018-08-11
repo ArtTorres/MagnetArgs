d4f273545f46d515d4004833a762e190c515751d
torrar <art.torres.alv@gmail.com>
2018-08-11 03:57:08 -0500

    Object for OptionSet tests

---
+++ MagnetArgs.Test/Models/CustomOptionSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagnetArgs.Test.Models
+{
+    class CustomOptionSet
+    {
+        [OptionSet]
+        public CustomObject Custom { get; set; }
+
+        [OptionSet]
+        public ComplexObject Complex { get; set; }
+    }
+}
