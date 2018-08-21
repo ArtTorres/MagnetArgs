dbe1e432794dafc4d51387e303283d39f47d8f83
torrar <art.torres.alv@gmail.com>
2018-08-21 04:07:54 -0500

    Summary specification in exceptions

--- MagnetArgs/Exceptions/IsRequiredException.cs
+++ MagnetArgs/Exceptions/IsRequiredException.cs
@@ -3,31 +3,31 @@
 namespace MagnetArgs
 {
     /// <summary>
-    /// 
+    /// Represents argument required errors ocurr during magnetize execution.
     /// </summary>
     public class IsRequiredException : MagnetException
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of <see cref="IsRequiredException"/> class.
         /// </summary>
         public IsRequiredException()
         {
         }
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of <see cref="IsRequiredException"/> class with error message.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public IsRequiredException(string message)
             : base(message)
         {
         }
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of <see cref="IsRequiredException"/> class with error message and a reference of a inner exception.
         /// </summary>
-        /// <param name="message"></param>
-        /// <param name="inner"></param>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception specified.</param>
         public IsRequiredException(string message, Exception inner)
             : base(message, inner)
         {
